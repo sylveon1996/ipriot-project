14ed3808cec496a39ec9125926985a8f86b26398
My Trinh <bloominoko@hotmail.com>
2023-12-04 02:32:07 +0800

    Added constructors and attributes to sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,13 @@
 class Display:
-    pass
+    def __init__(self,
+                 id,
+                 car_park,
+                 message="",
+                 is_on=False):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        return f'Display {self.id}:  Display {"is on" if self.is_on else "is off"}'
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,20 @@
 class Sensor:
-    pass
+    def __init__(self,
+                 id,
+                 car_park,
+                 is_active=False):
+
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"The {self.id} sensor {'is active' if self.is_active else 'not active'}"
+
+
+class EntrySensor(Sensor):
+    ...
+
+
+class ExitSensor(Sensor):
+    ...
